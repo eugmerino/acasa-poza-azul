c21120cf795f21df98f124443e7382632a57a78a
merino <merinoeug118@gmail.com>
2025-09-23 20:42:08 -0600

    [ADD] modulos de user y socio

--- project/urls.py
+++ project/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('comunidades/buscar/', views.community_search, name='community_search'),
     path('comunidades/crear/', views.community_create, name='community_create'),
     path('comunidades/editar/<int:pk>/', views.community_edit, name='community_edit'),
+    path('seguridad/usuarios/', views.users_list, name='users_list'),
+    path('seguridad/usuarios/buscar/', views.users_search, name='users_search'),
+    path("seguridad/usuarios/nuevo/", views.user_create_view, name="user_create"),
+    path('proyecto/socios/', views.partners_list, name='partners_list'),
+    path('proyecto/socios/buscar/', views.partners_search, name='partners_search'),
+    path("proyecto/socios/nuevo/", views.partner_create_view, name="partner_create"),
 ]
--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Project, Community, Community
+from .models import Project, Community, Partner
 from .forms import ProjectForm, CommunityForm
 from django.db.models import Q
 from django.core.paginator import Paginator
@@ -141,4 +141,138 @@ def community_edit(request, pk):
         request,
         "partials/community_form.html",
         {"form": form, "community": community}
+    )
+
+
+def users_list(request):
+    project = Project.objects.first()
+    query = request.GET.get('q', '') 
+    page_number = request.GET.get('page', 1)
+
+    per_page_options = [5, 10, 20, 50]
+    per_page = int(request.GET.get('per_page', per_page_options[1]))
+
+    users_list = Partner.objects.filter(is_superuser=False).order_by('-is_staff')
+    paginator = Paginator(users_list, per_page)
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'users_search_url': reverse('users_search'),
+        'page_obj': page_obj,
+        'query': query,
+        'per_page': per_page,
+        'per_page_options': per_page_options,
+        'project': project,
+    }
+
+    # Si la petición viene de HTMX => renderiza solo el contenido parcial
+    if request.headers.get('HX-Request'):
+        return render(request, "partials/users/users_content.html", context)
+
+    # Si es petición normal => renderiza toda la página
+    return render(request, "users/users.html", context)
+
+def users_search(request):
+    query = request.GET.get('q', '') 
+    page_number = request.GET.get('page', 1)
+    per_page = int(request.GET.get('per_page', per_page_options[1]))
+
+    users_list = Partner.objects.filter(
+        Q(is_superuser=False) & (Q(username__icontains=query) | Q(is_staff__icontains=query))
+    ).order_by('-is_staff')
+
+    paginator = Paginator(users_list, per_page)
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'partials/users/users_table.html',
+        {
+            'users_search_url': reverse('users_search'),
+            'page_obj': page_obj,
+            'query': query,
+            'per_page': per_page,
+            'per_page_options': per_page_options,
+        }
+    )
+
+def user_create_view(request):
+    project = Project.objects.first()
+    return render(
+        request,
+        "partials/users/user_form.html",  # parcial con el formulario
+        {"project": project}
+    )
+
+
+def partners_list(request):
+    project = Project.objects.first()
+    query = request.GET.get('q', '') 
+    page_number = request.GET.get('page', 1)
+
+    per_page_options = [5, 10, 20, 50]
+    per_page = int(request.GET.get('per_page', per_page_options[1]))
+
+    partners_list = Partner.objects.filter(is_superuser=False).order_by('-is_active')
+    paginator = Paginator(partners_list, per_page)
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'partners_search_url': reverse('partners_search'),
+        'page_obj': page_obj,
+        'query': query,
+        'per_page': per_page,
+        'per_page_options': per_page_options,
+        'project': project,
+    }
+
+    # Si la petición viene de HTMX => renderiza solo el contenido parcial
+    if request.headers.get('HX-Request'):
+        return render(request, "partials/partner/partners_content.html", context)
+
+    # Si es petición normal => renderiza toda la página
+    return render(request, "partner/partners.html", context)
+
+def partners_search(request):
+    query = request.GET.get('q', '') 
+    page_number = request.GET.get('page', 1)
+    per_page = int(request.GET.get('per_page', per_page_options[1]))
+
+    partners_list = Partner.objects.filter(
+        Q(is_superuser=False) &
+        (
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(community__name__icontains=query) |
+            Q(dui__icontains=query) |
+            Q(tel__icontains=query) |
+            (
+                Q(is_active=True)  if query.lower() == "activo" else
+                Q(is_active=False) if query.lower() == "inactivo" else
+                Q()  # vacío si no coincide
+            )
+        )
+    ).order_by('-is_active')
+
+    paginator = Paginator(partners_list, per_page)
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'partials/partner/partners_table.html',
+        {
+            'partners_search_url': reverse('partners_search'),
+            'page_obj': page_obj,
+            'query': query,
+            'per_page': per_page,
+            'per_page_options': per_page_options,
+        }
+    )
+
+def partner_create_view(request):
+    project = Project.objects.first()
+    return render(
+        request,
+        "partials/partner/partner_form.html",  # parcial con el formulario
+        {"project": project}
     )
